46ff71d328e85fcd2b8c0d7fe7db6efadb27edfc
Simonas Sankauskas <simonas.sank@gmail.com>
2018-10-03 11:15:38 +0300

    Add Author

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
 require 'sinatra'
 require 'json'
 
+# Author
+THE_AUTHOR = {
+  first_name: "Simonas",
+  last_name: "Sankauskas",
+  group: "IFF-5/3"
+}
+
+SAMPLE_AUTHOR = {
+  first_name: "Fist Name",
+  last_name: "Last Name",
+  group: "Any Group"
+}
+
+
+# Books
 OG_BOOK = { 
   id: 1,
   name: "The Orignal Book",
   summmary: "Super Cool Book About Things and Stuff.",
-  page_count: 3
+  page_count: 3,
+  author_list: [THE_AUTHOR]
 }
 
 SAMPLE_BOOK = {
@@ -13,14 +29,16 @@ SAMPLE_BOOK = {
   name: "Lorem ipsum",
   summmary: "Et dolor sunt perferendis in et ut et. Perspiciatis ipsum id consectetur
   iure voluptatum rer…",
-  page_count: 999999
+  page_count: 999999,
+  author_list: [SAMPLE_AUTHOR]
 }
 
 BRAVE_NEW_WORLD = {
   id: 3,
   name: "Brave New World",
   summmary: "Dude from the old world gets a bit of a culture shock.",
-  page_count: 311
+  page_count: 311,
+  author_list: [SAMPLE_AUTHOR]
 }
 
 UNIX_HATERS_HANDBOOK = {
@@ -28,7 +46,8 @@ UNIX_HATERS_HANDBOOK = {
   name: "The UNIXHATERS Handbook",
   summmary: "Two of the most famous products of Berkeley are LSD and Unix.
   I don’t think that is a coincidence.",
-  page_count: 360
+  page_count: 360,
+  author_list: [SAMPLE_AUTHOR]
 }
 
 BOOK_DB = [
@@ -82,11 +101,24 @@ get '/book/:id' do
   end
 end
 
+# Read
+get '/book/:id/authors' do
+  id = params['id'].to_i
+  book = BOOK_DB.find {|book| book[:id] == id }
+  if book
+    body book[:author_list].to_json
+  else
+    status 404
+    body "Which Book?"
+  end
+end
+
 # Read List
 get '/book' do
   BOOK_DB.to_json
 end
 
+# Delete
 delete '/book/:id' do
   id = params['id'].to_i
   book = BOOK_DB.find {|book| book[:id] == id } 
