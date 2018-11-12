ab08d34a63a144e5d9f8e190165bd8280c701e06
Simonas Sankauskas <simonas.sank@gmail.com>
2018-11-12 20:00:35 +0200

    Models, Seed, Controllers, Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    scope '/v1' do
+      scope '/author' do
+        get '/' => 'authors#index'
+        post '/' => 'authors#create'
+        scope '/:id' do
+          get '/' => 'authors#show'
+          put '/' => 'authors#update'
+          delete '/' => 'authors#destroy'
+        end
+      end
+      scope '/book' do
+        get '/' => 'books#index'
+        post '/' => 'books#create'
+        scope '/:id' do
+          get '/' => 'books#show'
+          put '/' => 'books#update'
+          delete '/' => 'books#destroy'
+          get '/authors' => 'books#authors'
+        end
+      end
+    end
+  end
 end
